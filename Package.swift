1d18e0faa93642a02435ca94981ba1a82499bb38
DB <bastidav@yahoo.fr>
2026-09-19 21:34:53 +0200

    Add initial Package.swift for ChatKit library

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "ChatKit",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "ChatKit", targets: ["ChatKit"]),
+    ],
+    targets: [
+        .target(name: "ChatKit"),
+        .testTarget(name: "ChatKitTests", dependencies: ["ChatKit"]),
+    ]
+)
